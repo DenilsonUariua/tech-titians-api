b88caaec0fe652c675a9d1fe6f7047501d57f6ee
Denilson Uariua <denuariua@gmail.com>
2023-10-12 16:44:26 +0200

    more cors

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000, https://sci-connect.netlify.app/"],
+    origin: ["http://localhost:3000, https://sci-connect.netlify.app"],
     methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
     credentials: true,
   })
