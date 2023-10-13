3394651e52991a7bbe60353d55e7219454b3c0a9
Denilson Uariua <denuariua@gmail.com>
2023-10-13 07:24:20 +0200

    fixed cors

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:4000",
+    origin: "http://localhost:3000",
     methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
     credentials: true,
   })
